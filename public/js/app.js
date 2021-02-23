e701a740ee2fa5fc6ff46b3365e7c896f543a290
Satyabati Goswami <satyabati.goswami@accenture.com>
2021-02-23 17:47:10 +0530

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Clien side java script loaded')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
